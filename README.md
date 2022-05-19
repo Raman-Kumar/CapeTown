4d704b29c8f3732afdf47c2af326e3f49bc0c6b7
Raman-Kumar <32980600+Raman-Kumar@users.noreply.github.com>
2022-05-20 01:42:38 +0530

    lifecycle of Activity

--- README.md
+++ README.md
@@ -1,3 +1,16 @@
 # CapeTown
 Lifecycle of Activity and Fragment
 
+When app start 
+
+and Activity opens - onCreate() , onStart(), onResume()
+
+if you press power button -  onPause(), onStop()
+
+and re-press power button and unlock screen - onRestart(), onStart(), onResume() 
+
+Press back button on a opened activity - onPause(), onStop(), onDestroy()
+
+So, open activity and press back button - 
+
+onCreate() , onStart(), onResume(), onPause(), onStop(), onDestroy()
--- app/src/main/java/com/example/capetown/MainActivity.kt
+++ app/src/main/java/com/example/capetown/MainActivity.kt
@@ -2,10 +2,42 @@ package com.example.capetown
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("lifecycle callbacks ", "onCreate()");
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("lifecycle callbacks ", "onStart()");
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("lifecycle callbacks ", "onResume()");
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("lifecycle callbacks ", "onPause()");
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("lifecycle callbacks ", "onStop()");
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("lifecycle callbacks ", "onDestroy()");
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.d("lifecycle callbacks ", "onRestart()");
     }
 }
